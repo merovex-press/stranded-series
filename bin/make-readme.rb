82618047ca6202790d444b865e8ca13fe886ef81
Ben W <dausha@gmail.com>
2019-07-07 10:58:32 -0400

    skip-changelog: hasty commit 2019-07-07 09:56:16

--- bin/make-readme.rb
+++ bin/make-readme.rb
@@ -11,55 +11,99 @@ actions = {
 
 content = File.open('README.md','r').read()
 
+def buildList(tag, template, list, order, content,section="")
+  # section = ""
+  list.sort_by { |k| k[order] }.each do |i|
+    section << template % i
+  end
+  return substitute(tag,content,section)
+end
+
 def substitute(tag,content,string)
-  return content.gsub(/<!-- #{tag} -->(.*)<!-- \/#{tag} -->/im, string)
+  s = "<!-- %s --><!-- auto-populated -->\n%s<!-- \/%s -->" % [tag, string, tag]
+  # puts s.inspect
+  return content.gsub(/<!-- #{tag} -->(.*)<!-- \/#{tag} -->/im, s)
 end
 def replace(file_path, tag, content)
   string = File.open(file_path,'r').read()
 
-  string = string.scan(/(<!-- #{tag} -->.*<!-- \/#{tag} -->)/imu).flatten.join("\n")
+  string = string.scan(/<!-- #{tag} -->(.*)<!-- \/#{tag} -->/imu).flatten.join("\n")
   return substitute(tag,content,string)
 end
 actions.each do |key, value|
   content = replace(value, key, content)
 end
 
+## Templates
+
+character_template =<<HERE;
+
+### %{name} (%{role})
+
+%{summary}
+
+Read more about [%{name}](%{filename})
+
+HERE
+
+season_template =<<HERE;
+| **[%{season}](%{filename})** | %{synopsis} |
+HERE
+
+
 ## Characters
 characters = []
 Dir.glob("./series-bible/03-Characters/*.md").each { |file|
   begin
     y = YAML.load_file(file)
-    characters << y if y.is_a? Hash
+    characters << {
+      :name => y['name'],
+      :role => y['role'],
+      :order => y['order'],
+      :summary => y['summary'],
+      :filename => file,
+    } if y.is_a? Hash
   rescue
   end
 }
-
-character_section = "<!-- character-section -->\n"
-characters.sort_by { |k| k["order"] }.each do |c|
-  character_section << "### #{c['name']} (#{c['role']})\n\n#{c['summary']}\n\n"
-end
-character_section << "<!-- /character-section -->"
-content = substitute("character-section",content,character_section)
 
 ## Seasons
 seasons = []
 Dir.glob("./series-bible/05-Treatments/**/*.md").each { |file|
   begin
     y = YAML.load_file(file)
-    y['filename'] = file
-    seasons << y if y.is_a? Hash
+    seasons << {
+      :season => y['season'].to_i,
+      :synopsis => y['synopsis'],
+      :filename => file,
+    } if y.is_a? Hash
   rescue
   end
 }
-# puts seasons.inspect
-season_section = "<!-- season-section -->\n| # | Synopsis |\n| :-: | - |\n"
-seasons.sort_by { |k| k["season"] }.each do |c|
-  season_section << "| **[#{c['season']}](#{c['filename']})** | #{c['synopsis']} |\n"
-end
-season_section << "<!-- /season-section -->"
+content = buildList(
+  "character-section",
+  character_template,
+  characters,
+  :order,
+  content
+)
+content = buildList(
+  "season-section",
+  season_template,
+  seasons,
+  :season,
+  content,
+  "| # | Synopsis |\n| :-: | - |\n"
+)
 
-
-content = substitute("season-section",content,season_section)
+# content = substitute("character-section",content,character_section)
+# content = substitute("season-section",content,season_section)
 # puts content
 
-File.open('README.md','w').write(content)
+puts "Building TOC"
+puts content.scan(/^##\s?(.*)\n/iu).flatten.map do |h|
+  "* [h](##{h})\n"
+end
+# puts string
+
+File.open('README-temp.md','w').write(content)
