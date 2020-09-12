30ff56a1b229339ffc1885dfae039f2198bc8328
Ben W <dausha@gmail.com>
2020-09-12 08:32:04 -0400

    tool: starting random name generator

---
+++ .verkilo/data/do.py
@@ -0,0 +1,30 @@
+import yaml
+
+file = "pa.yml"
+
+first = []
+last = []
+contents = []
+n = 0
+with open(file, 'r') as f:
+  for line in  f.readlines():
+    n += 1
+    f, l = line.strip().split(" ",2)
+    print(n,f,l)
+    # contents.append(l)
+    first.append(f)
+    last.append(l)
+
+first = list(dict.fromkeys(first))
+last = list(dict.fromkeys(last))
+first.sort()
+last.sort()
+
+names = {
+  'first': first,
+  'last': last
+}
+y = yaml.dump(names)
+print(y)
+with open('pennsylvania-names.yml','w') as f:
+  f.write(y)
